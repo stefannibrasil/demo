4f3d8373cfdaf75a186bafb7dda821d61c847a60
Stefanni Brasil <stefannibrasil@gmail.com>
2019-04-23 12:01:27 -0700

    Add pago and background job to charge order

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -2,11 +2,10 @@ require 'test_helper'
 
 class OrderMailerTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderMailer.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderMailer.received(FactoryBot.create(:order))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal ["stefannibrasil@gmail.com"], mail.to
+    assert_equal ["stefannibrasil@gmail.com"], mail.from
+    assert_match /1 x Programming with Ruby/, mail.body.encoded
   end
-
 end
--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,6 +1,7 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
 
   setup do
     @product = FactoryBot.create(:ruby)
@@ -21,6 +22,27 @@ class OrdersTest < ApplicationSystemTestCase
 
     select 'Check', from: 'pay_type'
 
-    assert_selector '#order_routing_number'
+    fill_in "Routing #", with: "123456"
+    fill_in "Account #", with: "987654"
+
+    perform_enqueued_jobs do
+      click_button "Place Order"
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+
+    assert_equal "Stefanni Brasil", order.name
+    assert_equal "123 Main Street",  order.address
+    assert_equal "stefannibrasil@gmail.com", order.email
+    assert_equal "Check", order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["stefannibrasil@gmail.com"], mail.to
+    assert_equal 'Stefanni Brasil <stefannibrasil@gmail.com>', mail[:from].value
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
   end
 end

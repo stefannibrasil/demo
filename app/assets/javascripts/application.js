1cc275509a7aa48adeae92623a41ecb3141256e1
Stefanni Brasil <stefannibrasil@gmail.com>
2019-04-26 13:32:38 -0700

    Add Semantic UI to app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery-3.1.1.min
+//= require semantic
+//= require semantic.min
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
